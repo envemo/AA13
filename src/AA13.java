eebbc514214b9f4f5f612b96d38903aa865f64fa
envem <envem@192.168.1.135>
2022-09-08 19:44:09 +0200

    AA13

--- src/AA13.java
+++ src/AA13.java
@@ -126,7 +126,7 @@ public class AA13 {
 			lineas.add("def date = new Date()");
 			lineas.add("");
 			lineas.add("pipeline {");
-			lineas.add("	stage {");
+			lineas.add("	stage ('HolaMundo') {");
 			lineas.add("		steps {");
 			lineas.add("			echo \"Hola Mundo! EL día de hoy es elDia \" + date.format('dd-MM-yyyy') + \". Este curso me hizo programar mas de lo que me hubiese gustado\"");
 			lineas.add("		}");
